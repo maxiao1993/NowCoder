8565b8fc3b6551882f8211885a06843adb376f36
maxiao1993 <soundwave_max@163.com>
2018-06-02 11:56:28 +0800

    complete MatrixChain

--- MatrixChain/myMatrix.cpp
+++ MatrixChain/myMatrix.cpp
@@ -2,18 +2,18 @@
 
 using namespace std;
 
-void MatrixChain(int *p, int n, int ** m, int **s);
+//void MatrixChain(int *p, int n, int ** m, int ** s);
 
+void MatrixChain();
+int s[7][7];
+int m[7][7];
+int p[7] = {30,35,15,5,10,20,25};
+int n = 6;
 int main()
 {
-	int p[7] = {30,35,15,5,10,20,25};
+	MatrixChain();
 
-	int m[7][7];
-	int s[7][7];
-
-
-	MatrixChain(p, 6, m, s);
-
+	cout << "m[i][j] is :"<<endl;
 	for (int i = 1; i <=6; i++)
 	{
 		for (int j = 1; j <= 6; j++)
@@ -23,10 +23,20 @@ int main()
 		
 		cout<<endl;
 	}
+	
+	cout << "s[i][j] is :"<<endl;
+	for (int i = 1; i <=6; i++)
+	{
+		for (int j = 1; j <=6; j++)
+		{
+			cout << s[i][j] <<"\t";
+		}
+		cout <<endl;
+	}
 	return 0;
 }
 
-void MatrixChain(int *p, int n, int **m, int **s)
+void MatrixChain()
 {
 	for(int i = 1; i <= n; i++)	//单一矩阵，无需计算
 		m[i][i] = 0;
