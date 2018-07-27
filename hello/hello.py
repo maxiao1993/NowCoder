93950df1afed5a459b784d8742078ce0d4da27f6
maxiao1993 <soundwave_max@163.com>
2018-07-27 15:24:29 +0800

    add hello/

---
+++ hello/hello.py
@@ -0,0 +1,11 @@
+print('Please input your nane.')
+name=input()
+print('Please input your age.')
+age=int(input())
+if name == 'Alice':
+    print('Hi Alice.')
+elif age < 12:
+    print('You are not Alice, kiddo.')
+else:
+    print('Hello,stranger.')
+    
---
+++ hello/while.py
@@ -0,0 +1,7 @@
+
+while True:
+    print('Please type your name.')
+    name = input()
+    if name == 'Your name':
+        break
+print('Thank you!')
